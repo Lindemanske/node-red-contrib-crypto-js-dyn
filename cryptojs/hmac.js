9701ea91056248dccbf694e1520c9bb7969eb77e
Lindemanske <lindemanske77@gmail.com>
2026-01-22 08:21:49 +0100

    Update hmac.js

--- cryptojs/hmac.js
+++ cryptojs/hmac.js
@@ -8,7 +8,7 @@ module.exports = function (RED) {
 		node.algorithm = config.algorithm;
 		node.key = config.key;
 
-		node.on('input', function (msg) {
+/*		node.on('input', function (msg) {
 			// check configurations
 			if(!node.algorithm || !node.key) {
 				// rising misconfiguration error
@@ -27,6 +27,25 @@ module.exports = function (RED) {
 
 				node.send(msg);
 			}
+		});*/
+		node.on('input', function (msg) {
+    		// Gebruik de sleutel uit de msg, of anders de sleutel uit de config
+    		var finalKey = msg.secretkey || node.key;
+
+    		if(!node.algorithm || !finalKey) {
+        		node.error("Missing configuration: algorithm or key.", msg);
+    		} else {
+        		if(msg.payload) {
+					// debugging message
+					node.debug('Creating a digest of payload using '+node.algorithm);
+            		// digest with CryptoJS
+            		msg.payload = CryptoJS[node.algorithm](msg.payload, finalKey).toString();
+        		} else {
+					// debugging message
+					node.trace('Nothing to digest: empty payload');
+				}
+        		node.send(msg);
+    		}
 		});
 	}
 
